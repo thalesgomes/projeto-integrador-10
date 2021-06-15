aabf7bc53f865a908f9e1618ca82b6adc8b15c53
weldocarvalho <weldocarvalho@outlook.com>
2021-06-15 19:03:48 -0300

    add rotas e renderizacao das outras telas

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@ const { request, response } = require('express');
 var express = require('express');
 var router = express.Router();
 
+router.get('/biblioteca', function(req, res) {
+  res.render('biblioteca');
+});
+
+router.get('/cadastro', function(req, res) {
+  res.render('cadastro-usuario');
+});
+
+router.get('/curso', function(req, res) {
+  res.render('criar-curso');
+});
+
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard-aluno');
+});
 
 router.get('/login', (resquest, response) => {
   response.render('login');
