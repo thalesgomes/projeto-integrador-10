00395e74b2c3fe8b7406b3e4722da658722e9801
weldocarvalho <weldocarvalho@outlook.com>
2021-06-21 14:19:27 -0300

    refatoracao: padronizando nomenclatura

---
+++ routes/indexRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+router.get('/usuarios/cadastro', (req, res) => {
+  res.render('usuarios_cadastro');
+});
+
+router.get('/usuarios/login', (req, res) => {
+  res.render('usuarios_login');
+});
+
+router.get('/aulas/biblioteca', (req, res) => {
+  res.render('aulas_biblioteca');
+});
+
+router.get('/aulas/topicos', (req, res) => {
+  res.render('aulas_topicos');
+});
+
+router.get('/aulas/edicao', (req, res) => {
+  res.render('aulas_edicao');
+});
+
+router.get('/aulas/emcurso', (req, res) => {
+  res.render('aulas_em_curso');
+});
+
+router.get('/forum', (req, res) => {
+  res.render('forum');
+});
+
+router.get('/forum/topicos', (req, res) => {
+  res.render('forum_topicos');
+});
+
+module.exports = router;
