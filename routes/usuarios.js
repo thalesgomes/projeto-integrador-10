0112803bc4416ad31da1150076858665ef7a89eb
claudionono14 <claudio.nono14@gmail.com>
2021-06-30 14:10:03 -0300

    adicionando rotas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const usuariosController = require('../controllers/usuariosController')
 
+/** http://localhost:3000/usuarios/cadastro */
+router.get('/cadastro', usuariosController.cadastro);
+router.post('/cadastro', usuariosController.salvar);
+
+/** http://localhost:3000/usuarios/login */
+router.get('/login', usuariosController.login);
+router.post('/login', usuariosController.autenticacao);
 
 
 module.exports = router;
