a4fedde2605bbbaf495216ff506399f2f5168ad8
claudionono14 <claudio.nono14@gmail.com>
2021-06-30 13:48:12 -0300

    adicionando arquivo de rotas usuarios.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/indexRouter');
+const usuariosRouter = require('./routes/usuarios');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
