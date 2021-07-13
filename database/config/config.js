8c1f6ad2c03094eac72b574872377737e248b67b
weldocarvalho <weldocarvalho@outlook.com>
2021-07-13 17:26:54 -0300

    correçao: database config

--- database/config/config.js
+++ database/config/config.js
@@ -8,7 +8,12 @@ module.exports = {
     define: {
       timestamps: true,
       underscored: true,
-    }
+      underscoredAll: true,
+    },
+    dialectOptions: {
+      timezone: 'America/Sao_Paulo'
+    },
+    timezone: 'America/Sao_Paulo'
   }
 }
 
