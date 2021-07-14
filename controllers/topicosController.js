f749770371288dbfe16c1348af657c37b51c51da
weldocarvalho <weldocarvalho@outlook.com>
2021-07-14 15:37:00 -0300

    criaçao metodo consultar topicos

--- controllers/topicosController.js
+++ controllers/topicosController.js
@@ -2,6 +2,14 @@ const { Disciplina } = require('../database/models');
 const { Topico } = require('../database/models');
 
 const topicosController = {
+    index: async (req, res) => {
+        const { disciplina_id } = req.params;
+        const disciplina = await Disciplina.findByPk(disciplina_id, {
+            include: { association: 'topicos' }
+        });
+        res.json(disciplina);
+    },
+
     store: async (req, res) => {
         const { disciplina_id } = req.params;
         const { id, nome } = req.body;
@@ -9,7 +17,7 @@ const topicosController = {
         const disciplina = await Disciplina.findByPk(disciplina_id);
 
         if (!disciplina) {
-            return res.status(400).json({ erro: 'usuário não encontrado' })
+            return res.status(400).json({ erro: 'disciplina não encontrada' })
         }
 
         const topico = await Topico.create({
--- routes/topicosRouter.js
+++ routes/topicosRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const topicosController = require('../controllers/topicosController');
 
+router.get('/:disciplina_id/topicos', topicosController.index);
 router.post('/:disciplina_id/topicos', topicosController.store);
 
 module.exports = router;
