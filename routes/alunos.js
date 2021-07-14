953d0da3e2266ed29da31b0c3936465b5e5e054f
weldocarvalho <weldocarvalho@outlook.com>
2021-07-14 12:17:52 -0300

    substituiçao dos arquivos

--- routes/alunos.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const alunosController = require('../controllers/alunosController');
-
-router.post('/', alunosController.store);
-
-module.exports = router;
