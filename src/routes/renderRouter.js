3d2ae8ea8c9cb79e25e22a719284e71b47f6251b
weldocarvalho <weldocarvalho@outlook.com>
2021-07-16 18:46:15 -0300

    refatorando: rotas de renderizacao

--- src/routes/renderRouter.js
+++ src/routes/renderRouter.js
@@ -2,16 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
-
 router.get('/aulas/biblioteca', (req, res) => {
   res.render('aulas_biblioteca');
-});
-
-router.get('/aulas/topicos', (req, res) => {
-  res.render('aulas_topicos');
 });
 
 router.get('/aulas/edicao', (req, res) => {
@@ -22,12 +14,28 @@ router.get('/aulas/emcurso', (req, res) => {
   res.render('aulas_em_curso');
 });
 
+router.get('/aulas/topicos', (req, res) => {
+  res.render('aulas_topicos');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
 router.get('/forum', (req, res) => {
   res.render('forum');
 });
 
 router.get('/forum/topicos', (req, res) => {
   res.render('forum_topicos');
+});
+
+router.get('/usuarios/cadastro', (req, res) => {
+  res.render('usuarios_cadastro');
+});
+
+router.get('/usuarios/login', (req, res) => {
+  res.render('usuarios_login');
 });
 
 module.exports = router;
