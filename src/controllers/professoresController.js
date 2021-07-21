d0ecd55acf0298a91b0a18b574551c15eb97c776
weldocarvalho <weldocarvalho@outlook.com>
2021-07-21 13:19:47 -0300

    metodo para cadastro de professores

--- src/controllers/professoresController.js
+++ src/controllers/professoresController.js
@@ -1,4 +1,6 @@
-const { Disciplina, Professor } = require('../database/models/index');
+const bcrypt = require('bcrypt');
+const { uuid } = require('uuidv4');
+const { Disciplina, Professor } = require('../database/models');
 
 const professoresController = {
 
@@ -10,20 +12,32 @@ const professoresController = {
 
   show: async (req, res) => {
     const professores = await Professor.findAll();
-
     res.json(professores);
   },
 
   store: async (req, res) => {
+    const id = uuid();
+
     const {
-      id, nome, sobrenome, email, senha, categoria,
+      nome,
+      sobrenome,
+      email,
+      senha,
+      categoria,
     } = req.body;
 
+    const hashSenha = bcrypt.hashSync(senha, 10);
+
     const professor = await Professor.create({
-      id, nome, sobrenome, email, senha, categoria,
+      id,
+      nome,
+      sobrenome,
+      email,
+      senha: hashSenha,
+      categoria,
     });
 
-    res.json(professor);
+    return res.json(professor);
   },
 };
 
