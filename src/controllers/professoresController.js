bc84b9299673f651b2919b4cacf2fdd3a3c13a28
weldocarvalho <weldocarvalho@outlook.com>
2021-07-22 00:14:53 -0300

    exp-session e corrigindo implementaçao (professores)

--- src/controllers/professoresController.js
+++ src/controllers/professoresController.js
@@ -1,44 +1,15 @@
-const bcrypt = require('bcrypt');
-const { uuid } = require('uuidv4');
-const { Disciplina, Professor } = require('../database/models');
+const { Disciplina } = require('../database/models');
 
 const professoresController = {
 
   dashboardRender: async (req, res) => {
+    const { id, nome } = req.session.professor;
+
     const disciplinas = await Disciplina.findAll();
-    const { id_professor } = req.params;
-    res.render('dashboard_professor', { disciplinas, id_professor });
+
+    return res.render('dashboard_professor', { nome, id, disciplinas });
   },
 
-  show: async (req, res) => {
-    const professores = await Professor.findAll();
-    res.json(professores);
-  },
-
-  store: async (req, res) => {
-    const id = uuid();
-
-    const {
-      nome,
-      sobrenome,
-      email,
-      senha,
-      categoria,
-    } = req.body;
-
-    const hashSenha = bcrypt.hashSync(senha, 10);
-
-    const professor = await Professor.create({
-      id,
-      nome,
-      sobrenome,
-      email,
-      senha: hashSenha,
-      categoria,
-    });
-
-    return res.json(professor);
-  },
 };
 
 module.exports = professoresController;
--- src/routes/professoresRouter.js
+++ src/routes/professoresRouter.js
@@ -4,21 +4,6 @@ const professoresController = require('../controllers/professoresController');
 const router = express.Router();
 
 // RENDERIZAÇÃO DO DASHBOARD
-router.get('/dashboard/professores/:id_professor', professoresController.dashboardRender);
-
-// OBTER TODOS OS PROFESSORES
-router.get('/professores/', professoresController.show);
-
-// OBTER APENAS UM PROFESSOR
-// router.get('/professores/:id_professor/', professoresController.index);
-
-// CADASTRAR UM PROFESSOR
-router.post('/professores/', professoresController.store);
-
-// ATUALIZAR UM PROFESSOR
-// router.post('/professores/:id_professor/', professoresController.update);
-
-// DELETAR UM PROFESSOR
-// router.post('/professores/:id_professor/', professoresController.delete);
+router.get('/professor/dashboard', professoresController.dashboardRender);
 
 module.exports = router;
