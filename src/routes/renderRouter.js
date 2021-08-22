37ce6cbac0e173270b01c379716db45e66b25e75
weldocarvalho <weldocarvalho@outlook.com>
2021-08-22 15:28:13 -0300

    views: topicos e aulas_em_curso

--- src/routes/renderRouter.js
+++ src/routes/renderRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/index', (req, res) => {
-  res.render('pages/index');
+router.get('/topicos', (req, res) => {
+  res.render('pages/topicos_professor');
+});
+
+router.get('/aulas', (req, res) => {
+  res.render('pages/aulas_em_curso');
 });
 
 module.exports = router;
