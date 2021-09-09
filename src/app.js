ce1f03200fc0fcb7c1be6bec8bd23f2d7276c725
weldocarvalho <weldocarvalho@outlook.com>
2021-09-09 10:42:31 -0300

    css: atualizando diretório

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // public directory setup
-app.use(express.static(path.resolve(__dirname, '../', 'public')));
+app.use(express.static(path.resolve(__dirname, '../', 'frontend')));
 
 // express-session setup
 app.use(
