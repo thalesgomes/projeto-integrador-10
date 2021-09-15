582c79d12651f3c041e7b807f83fd9f2a07e48e9
weldocarvalho <weldocarvalho@outlook.com>
2021-09-15 09:00:28 -0300

    (bancoConfig) correção no process.env

--- src/database/config/connectionConfig.js
+++ src/database/config/connectionConfig.js
@@ -4,8 +4,8 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: process.env.HOST,
-  port: process.env.PORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   define: {
     timestamps: true,
